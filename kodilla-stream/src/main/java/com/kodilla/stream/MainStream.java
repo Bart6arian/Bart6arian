5a1a0128e7fdb763157cbe777ff682ae0277dd3e
Bartosz Zemsta <zemstabartosz@gmail.com>
2021-08-18 19:21:59 +0200

    user

--- kodilla-stream/src/main/java/com/kodilla/stream/MainStream.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/MainStream.java
@@ -1,12 +1,18 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.iterate.NumberGenerator;
+import com.kodilla.stream.person.People;
+
+import java.util.Locale;
 
 public class MainStream {
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
 
-        System.out.println("Using Stream to generate evens from 3 to 23");
-        NumberGenerator.generateEven(23);
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ')+2)+".")
+                .filter(s -> s.charAt(0) == 'M')
+                .forEach(System.out::println);
     }
 }
