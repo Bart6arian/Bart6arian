152bc2402eaee708fcfd54622069a5fa586f11ab
Bartosz Zemsta <zemstabartosz@gmail.com>
2021-08-21 08:43:31 +0200

    fresh

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,18 +1,14 @@
 package com.kodilla.stream.array;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static OptionalDouble getAverage(int[]numbers) {
-        List<Double> number = new ArrayList<>();
-        for(int x = 0; x <= 20; x++) {
-            number.add((double) x);
-        }
-        IntStream.range(0, number.size()).forEach(System.out::println);
-        OptionalDouble average = IntStream.range(0, number.size()).average();
+
+        IntStream.range(1, 20).forEach(System.out::println);
+        OptionalDouble average = IntStream.range(1, numbers.length)
+                .average();
         return average;
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 import static com.kodilla.stream.array.ArrayOperations.getAverage;
@@ -12,16 +11,17 @@ public class ArrayOperationsTestSuite {
 
     @Test
     void testGetAverage() {
-        int[]numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8,9,10, 11, 12, 13, 14, 15, 16, 17, 18, 19 ,20};
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8,9,10, 11, 12, 13, 14, 15, 16, 17, 18, 19 ,20};
         //when
         getAverage(numbers);
         //then
-        IntStream arrayOperations1 = IntStream.range(0, 20);
-        assertEquals(20, arrayOperations1);
+        IntStream arrayOperations1 = IntStream.range(1, 20);
 
-        OptionalDouble average = IntStream.range(0, 20).average();
-        assertEquals(0, average);
-
+        double expected = 10;
+        double arrayOperations2 = IntStream.range(1, numbers.length)
+                        .average()
+                        .getAsDouble();
+        assertEquals(expected, arrayOperations2);
 
     }
 }
