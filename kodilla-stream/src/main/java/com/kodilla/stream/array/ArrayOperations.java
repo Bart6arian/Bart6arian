06a2e2041ff1800ddcad0a8610931b932ecdec73
Bartosz Zemsta <zemstabartosz@gmail.com>
2021-08-21 13:14:36 +0200

    fresh

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,11 +4,10 @@ import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
-    static OptionalDouble getAverage(int[]numbers) {
+    static void getAverage(int[]numbers) {
 
-        IntStream.range(1, 20).forEach(System.out::println);
-        OptionalDouble average = IntStream.range(1, numbers.length)
-                .average();
-        return average;
+        IntStream.range(0, numbers.length).forEach(number -> System.out.println(numbers[number]));
+        OptionalDouble average = IntStream.of(numbers).average();
+        System.out.println(average);
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 import static com.kodilla.stream.array.ArrayOperations.getAverage;
@@ -11,17 +12,14 @@ public class ArrayOperationsTestSuite {
 
     @Test
     void testGetAverage() {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8,9,10, 11, 12, 13, 14, 15, 16, 17, 18, 19 ,20};
-        //when
-        getAverage(numbers);
-        //then
-        IntStream arrayOperations1 = IntStream.range(1, 20);
+        int[] numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8,9,10, 11, 12, 13, 14, 15, 16, 60, 30, 10 ,20};
 
-        double expected = 10;
-        double arrayOperations2 = IntStream.range(1, numbers.length)
-                        .average()
-                        .getAsDouble();
-        assertEquals(expected, arrayOperations2);
+        //then
+        IntStream.range(0, numbers.length)
+                .forEach(number -> System.out.println(numbers[number]));
+
+        OptionalDouble average = IntStream.of(numbers).average();
+        assertEquals(0, average);
 
     }
 }
