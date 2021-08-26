a9ee7934777dd4bf680218a03e146df019f91d42
Bartosz Zemsta <zemstabartosz@gmail.com>
2021-08-26 14:58:18 +0200

    commit

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -2,10 +2,16 @@ package com.kodilla.exception.test;
 
 public class FirstChallenge {
 
-    public double divide(double a, double b) {
-        if (b == 0) {
+    public double divide(double a, double b) throws ArithmeticException {
+        return  a / b;
+    }
+
+    public static void main(String[] args)  {
+        FirstChallenge firstChallenge = new FirstChallenge();
+        double result = firstChallenge.divide(3, 0);
+
             try {
-                return a/b;
+                System.out.println(result);
             } catch (ArithmeticException e) {
 
                 System.out.println("Illegal move");
@@ -13,15 +19,7 @@ public class FirstChallenge {
 
                 System.out.println("You can not divide by 0");
             }
-        }
-        return  a / b;
-    }
 
-    public static void main(String[] args) throws ArithmeticException {
-        FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3, 0);
-
-        System.out.println("Don't try this at home! \n\nResults is following: ");
-        System.out.println(result);
+        System.out.println("Don't try this at home!");
     }
 }
