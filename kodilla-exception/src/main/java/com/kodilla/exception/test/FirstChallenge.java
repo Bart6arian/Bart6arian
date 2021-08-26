08c7f497ebe6cc2ef69be9745115cad9265a7234
Bartosz Zemsta <zemstabartosz@gmail.com>
2021-08-26 05:11:56 +0200

    commit

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -2,7 +2,7 @@ package com.kodilla.exception.test;
 
 public class FirstChallenge {
 
-    public double divide(double a, double b) throws ArithmeticException {
+    public double divide(double a, double b) {
         if (b == 0) {
             try {
                 return a/b;
@@ -13,22 +13,11 @@ public class FirstChallenge {
 
                 System.out.println("You can not divide by 0");
             }
-        } else {
-            if(a == 0) {
-                try {
-                    return a/b;
-                } catch (ArithmeticException e) {
-
-                    System.out.println("Impossible");
-                } finally {
-
-                    System.out.println("You can not divide 0");
-                }
-            }
         }
         return  a / b;
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws ArithmeticException {
         FirstChallenge firstChallenge = new FirstChallenge();
         double result = firstChallenge.divide(3, 0);
         double result2 = firstChallenge.divide(0, 5);
