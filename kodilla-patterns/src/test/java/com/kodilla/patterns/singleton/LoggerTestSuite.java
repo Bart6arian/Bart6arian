405d58d4089ed27dfa3e6472f13e8331d4d96d92
Bartosz Zemsta <zemstabartosz@gmail.com>
2021-10-12 18:13:40 +0200

    patterns I

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,30 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LoggerTestSuite {
+
+    private static Logger logger;
+
+    @BeforeAll
+    public static void saveToLog() {
+        logger = Logger.INSTANCE;
+        String log = "something";
+        logger.log(log);
+    }
+
+    @Test
+    public void lastLogTest() {
+        //given
+        //when
+        String lastLog = logger.getLastLog();
+        System.out.println("Last log: "+lastLog);
+        //then
+        assertEquals("something", lastLog);
+    }
+
+}
--- kodilla-spring1/src/main/java/com/kodilla/spring/portfolio/TaskList.java
+++ kodilla-spring1/src/main/java/com/kodilla/spring/portfolio/TaskList.java
@@ -1,6 +1,5 @@
 package com.kodilla.spring.portfolio;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class TaskList {
